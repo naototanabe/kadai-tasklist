f7f16719c6972cea9a6e702549e892b46d929817
naoto.tanabe <kotikame00@gmail.com>
2021-12-17 11:22:08 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,13 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function() {
+    return view('welcome');
+});
 
 
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
